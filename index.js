3a690106392b5b505d03a3b6c3f8628137db4bc7
Sahil <84180363+saahil03@users.noreply.github.com>
2022-07-12 15:54:12 +0530

    JavaScript

---
+++ index.js
@@ -0,0 +1,9 @@
+/* Alert box setup*/
+
+function myFunction() {
+
+  alert('Hii ' + document.getElementById('name').value + ', Thank you for visiting \n\nYour password is - ' + document.getElementById('password').value);
+
+  alert('If you really like it, Upvote my code and Share it as many as you can.. \nKeep Supporting... \n\nThank you😊');
+
+}
